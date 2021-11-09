6ffdcd314bd4096c9927b2b35a83b36c8f55857f
Max Blancarte <blancarte@gmail.com>
2021-11-09 15:54:01 -0800

    feat: added Prisma and db connection
    Added Prisma DB conneciton along with initial migration for database.

---
+++ seed/users.ts
@@ -0,0 +1,17 @@
+// users product seed
+export const users = [
+  {
+    email: 'admin@admin.com',
+    password: 'admin',
+    name: 'admin',
+    lastName: 'Doe',
+    role: 'admin',
+  },
+  {
+    email: 'test@test.com',
+    password: 'Testing01!',
+    name: 'test',
+    lastName: 'tester',
+    role: 'tester',
+  },
+];
