8019932573c82a807f9129abc75aeb5eb035a0f3
Max Blancarte <blancarte@gmail.com>
2021-11-15 09:34:30 -0800

    fix: localauth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import RegisterDto from './dto/register.dto';
 import { Auth } from './entity/auth.entity';
+// import { LocalAuthGuard } from './local.auth.guard';
 import { LocalAuthGuard } from './local.auth.guard';
 
 @Controller('auth')
@@ -13,8 +14,10 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  //Local Auth Guard requires only email and password
   @UseGuards(LocalAuthGuard)
-  async register(@Body() registrationData: RegisterDto) {
+  async register(@Body() registrationData: RegisterDto, @Request() req) {
+    console.log(req);
     return this.authService.register(registrationData);
   }
 
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,21 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
-    });
+    super();
   }
   async validate(email: string, password: string): Promise<any> {
     const user = this.authService.getAuthenticatedUser(email, password);
     if (!user) {
       throw new UnauthorizedException();
     }
+
     return user;
   }
 }
